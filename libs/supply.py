f686cd9f89cec480e681c78a1de90392b48ae2cc
goshiz <goshiz@goshiz.io>
2020-07-07 11:26:46 +0200

    add max on last block

--- libs/supply.py
+++ libs/supply.py
@@ -55,8 +55,12 @@ class CirculationSupplyResource:
               resp.status=falcon.HTTP_401
               return 
         firstBlock=self.mydb.find_first("blocks")
+        lastBlock=self.mydb.find_last("blocks")
         if firstBlock is not None and firstBlock['header']['timestamp']>dt_from:
             dt_from=firstBlock['header']['timestamp']
+        if lastBlock is not None and lastBlock['header']['timestamp']<dt_to:
+            dt_to=lastBlock['header']['timestamp']
+        
         ts_to=calendar.timegm(dt_to.timetuple())
         ts_from=calendar.timegm(dt_from.timetuple())
         #We need ~100 pts of data, so each points will be seperated by ts_diff/100
