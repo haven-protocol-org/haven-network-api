4f78ee30e19af0ce292c1cdcf9bc82f19f54e407
Goshiz <goshiz@goshiz.io>
2021-04-07 18:53:26 +0200

    update cmd bash

--- cmd/mainnet.sh
+++ cmd/mainnet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export hv_mongo_url='mongodb://localhost:27017/?retryWrites=true&w=majority'
-export hv_mongo_db='haven_mainnet'
-export hv_daemon_url='https://vault.havenprotocol.org:443'
+export hv_mongo_db='haven_mainnet2'
+export hv_daemon_url='http://127.0.0.1:17750'
 #export hv_resetrates=true
 export hv_debug=true
 
--- cmd/testnet.sh
+++ cmd/testnet.sh
@@ -2,7 +2,7 @@
 
 export hv_mongo_url='mongodb://localhost:27017/?retryWrites=true&w=majority'
 export hv_mongo_db='haven_testnet'
-export hv_daemon_url='https://vault-testnet.havenprotocol.org:443'
+export hv_daemon_url='http://localhost:27750'
 #export hv_resetrates=true
 export hv_debug=true
 
